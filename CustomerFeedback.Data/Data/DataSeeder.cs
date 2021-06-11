d441347253da579ea060be41f60560bc7858224b
Paolo <zilofi.yahoo.it>
2021-06-11 12:15:38 +0100

    Update DataSeeder.cs
    adjustments

--- CustomerFeedback.Data/Data/DataSeeder.cs
+++ CustomerFeedback.Data/Data/DataSeeder.cs
@@ -11,36 +11,12 @@ namespace CustomerFeedback.Data.Data
             {
                 var feedbacks = new List<Feedback>()
                 {
-                    //new User { /*Id = 1,*/ Name = "John", Email = "john@john.com" },
-                    //new User { /*Id = 2,*/ Name = "Michael", Email = "michael@michael.com" }
                     new Feedback { /*Id = 1,*/ FirstName = "John", Lastname = "surname1", Comment = "commentLineline" },
                     new Feedback { /*Id = 2,*/ FirstName = "Peter", Lastname = "surname2", Comment = "commentLineline2" }
                 };
                 context.Feedback.AddRange(feedbacks);
                 context.SaveChanges();
-            }
-
-            //if (!context.Recipes.Any())
-            //{
-            //    var recipes = new List<Recipe>()
-            //{
-            //    new Recipe { /* Id = 1 */ Name = "Pizza", Description = "A random description from a pizza", Difficulty = RecipeDifficulty.MEDIUM, ImagePath = "https://upload.wikimedia.org/wikipedia/commons/a/a4/Pizza.jpg", Time = 30, UserId = 1 }
-            //};
-            //    context.Recipes.AddRange(recipes);
-            //    context.SaveChanges();
-            //}
-
-            //if (!context.Ingredients.Any())
-            //{
-            //    var ingredients = new List<Ingredient>()
-            //{
-            //    new Ingredient { /* Id = 1 */ Name = "Cheese", Amount = 1, RecipeId = 1 },
-            //    new Ingredient { /* Id = 2 */ Name = "Tomato", Amount = 2, RecipeId = 1 }
-            //};
-            //    context.Ingredients.AddRange(ingredients);
-            //    context.SaveChanges();
-            //}
+            }  
         }
     }
-//}
 }
